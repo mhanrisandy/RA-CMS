4d3c8b8b526738264833a2548d64a1172d84d80f
Risman Arisandi <mhanrisandy@gmail.com>
2021-08-04 13:06:48 +0800

    mengubah control.php

--- login/control.php
+++ login/control.php
@@ -1,15 +1,15 @@
 <?php
-// Bagian Home
+// Home
 if ($_GET['module']=='home'){ 
   include "modul/mod_home/home.php";
 }
 
-// Bagian User
+// modul lain
 elseif ($_GET['module']=='nama_modul'){
     include "modul/nama_modul/nama_modul.php";
 }
 
-// Apabila modul tidak ditemukan
+// modul tanpa kondisi
 else{
      if (file_exists("modul/mod_". $_GET['module'] ."/". $_GET['module'] .".php")){
        include "modul/mod_". $_GET['module'] ."/". $_GET['module'] .".php";
